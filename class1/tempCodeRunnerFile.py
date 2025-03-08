17eceda65e5c84bf65cfdabc0fcb06143e7a3e60
Olivia1214 <olivianaomiko@gmail.com>
2025-03-08 09:42:46 +0800

    class1

--- class1/tempCodeRunnerFile.py
+++ class1/tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-# class1.py
-# ctrl + ?可以切換滑鼠框選的範圍成為註解
+# 基本型態
+print(1)  # 整數 int
+print(1.0)  # 浮點數 float, 只要有小數點就是浮點數
+print("Hello World")  # 字串 str, 用雙引號""或單引號''包起來的都是字串
+print(True)  # 布林值 bool, 第一個字母要大寫
+print(None)  # 空值 NoneType
