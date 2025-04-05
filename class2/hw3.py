ad68539faa46b66ce88000289be0f6456139109c
Olivia1214 <olivianaomiko@gmail.com>
2025-04-05 19:19:35 +0800

    hw3

--- class2/hw3.py
+++ class2/hw3.py
@@ -14,3 +14,9 @@ w = input("請輸入體重(公斤):")
 w = float(w)
 bmi = w / r**2
 print("你的BMI為" + str(bmi))
+if bmi >= 14.8 and bmi <= 20.7:
+    print("體重正常")
+elif bmi < 14.8:
+    print("體重過輕")
+else:
+    print("體重過重")
