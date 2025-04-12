97a3c4c2dcebaabb9905c8a0367c1d615c955645
Olivia1214 <olivianaomiko@gmail.com>
2025-04-12 09:52:08 +0800

    turtle

--- class4/tempCodeRunnerFile.py
+++ class4/tempCodeRunnerFile.py
@@ -4,10 +4,10 @@ t.shape("circle")
 t.color("red")
 t.speed(5)
 
-for i in range(0, 60):
+for i in range(0, 58):
     t.stamp()
     t.penup()
-    t.right(20)
-    t.forward(i+10)
-    
+    t.right(25)
+    t.forward(i + 20)
+
 t.done()
