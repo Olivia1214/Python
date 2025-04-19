13878072e3d2f5c462b242d87e8c15b05047ceef
Olivia1214 <olivianaomiko@gmail.com>
2025-04-19 10:38:12 +0800

    hw5

--- class5/hw5.py
+++ class5/hw5.py
@@ -19,3 +19,20 @@ for i in range(8):
 
 t.home()
 t.done()
+
+import turtle as t
+import time
+
+t.left(90)
+
+t.speed(0)
+
+for j in range(60):
+    t.right(6 * j)
+    t.forward(100)
+    time.sleep(1)
+    t.home()
+    t.left(90)
+    t.clear()
+
+t.done()
--- class5/tempCodeRunnerFile.py
+++ class5/tempCodeRunnerFile.py
@@ -1,17 +1,16 @@
 import turtle as t
+import time
 
-t.color("black")
-t.speed(5)
+t.left(84)
 
-t.penup()
-t.forward(200)
+t.speed(0)
 
-for i in range(8):
-    t.stamp()
-    t.penup()
-    t.right(135)
-    t.forward(160)
+for j in range(60):
+    t.right(6 * j)
+    t.forward(100)
+    time.sleep(1)
+    t.home()
     t.left(90)
+    t.clear()
 
-t.home()
 t.done()
