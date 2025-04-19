bfcd6871fd68daf0c702f1e44dfef0a1caed8c1d
Olivia1214 <olivianaomiko@gmail.com>
2025-04-19 11:57:17 +0800

    class6

--- class6/class6.py
+++ class6/class6.py
@@ -34,9 +34,6 @@ while i < 10:
 
 n = int(input("請輸入密碼："))
 
-while n == 1214:
-    print("歡迎光臨")
-
-while n > 1214 or n < 1214:
-    print("請重新輸入")
+while n != 1214:
     n = int(input("請輸入密碼："))
+print("歡迎光臨")
--- class6/tempCodeRunnerFile.py
+++ class6/tempCodeRunnerFile.py
@@ -1,10 +1,5 @@
-
 n = int(input("請輸入密碼："))
 
-while n == 1214:
-    print("歡迎光臨")
-
-while n > 1214 or n < 1214:
-    print("請重新輸入")
+while n != 1214:
     n = int(input("請輸入密碼："))
-    
+print("歡迎光臨")    
