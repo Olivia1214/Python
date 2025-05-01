387e6afb87a4d243ab32552a72a3d88f0ffa4d49
Olivia1214 <olivianaomiko@gmail.com>
2025-05-01 20:28:14 +0800

    hw7

--- class7/hw7.py
+++ class7/hw7.py
@@ -21,4 +21,6 @@ s = int(input("請輸入起始數字："))
 e = int(input("請輸入結束數字："))
 
 for i in range(s, e + 1):
-    
+    for j in range(2, i - 1):
+        if i % j == 0:
+            print(i)
--- class7/tempCodeRunnerFile.py
+++ class7/tempCodeRunnerFile.py
@@ -1,7 +1,11 @@
 s = int(input("請輸入起始數字："))
 e = int(input("請輸入結束數字："))
 
+ans = "是質數"
+
 for i in range(s, e + 1):
-    for j in range(2, i):
+    for j in range(2, i - 1):
         if i % j == 0:
-            print(i)
+            ans = "不是質數"
+            if ans == "是質數":
+                print(i)
