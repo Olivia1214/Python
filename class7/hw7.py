75e1bcfc400c1ea5e7154c38e4c9639e011234fc
Olivia1214 <olivianaomiko@gmail.com>
2025-05-03 10:08:43 +0800

    hw7

--- class7/hw7.py
+++ class7/hw7.py
@@ -21,7 +21,7 @@ s = int(input("請輸入起始數字："))
 e = int(input("請輸入結束數字："))
 
 for i in range(s, e + 1):
-    for j in range(2, i - 1):
+    for j in range(2, i):
         if i % j == 0:
             break
     else:
