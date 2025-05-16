b806bb1f6996c7c199107992ee804bfdd846ab3c
Olivia1214 <olivianaomiko@gmail.com>
2025-05-16 21:23:44 +0800

    hw9

--- class9/tempCodeRunnerFile.py
+++ class9/tempCodeRunnerFile.py
@@ -1,16 +1,13 @@
-# 升級版果汁機(用List來儲存資料)(while迴圈中不可出現果汁選項)
-j_l = ["蘋果汁", "柳橙汁", "葡萄汁", "楊桃汁", "系統關閉"]
-b = 0
-while b != len(j_l):
-    for i in range(len(j_l)):
-        print(f"{i + 1}.{j_l[i]}")
-    try:
-        b = int(input("請輸入你的選擇(編號): "))
-        if b < len(j_l):
-            print(f"你的選擇是{j_l[b-1]}")
-        elif b == len(j_l):
-            break
-        else:
-            print("輸入錯誤, 請重新輸入")
-    except:
-        print("輸入錯誤, 請重新輸入")
+import random as r
+
+# 數字猜測遊戲(需提示再大一點或小一點)(需要提示縮小過後的輸入範圍)
+n = r.randint(1, 100)
+g = int(input("請輸入你的猜測(1~100):"))
+while g != n:
+    if n > g:
+        print("再大一點")
+    elif n < g:
+        print("再小一點")
+    g = int(input("請輸入你的猜測(1~100):"))
+
+print("恭喜猜中了")
